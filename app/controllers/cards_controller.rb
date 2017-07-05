f89fc082f7f8dd5ce3a0a7ed158c8cc1a3521dd5
devico <clamdm@mail.ru>
2017-07-06 01:24:00 +0300

    add actions to cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,9 @@
 class CardsController < ApplicationController
   before_action :require_login, :only => :new
+  before_action :set_card, only: [:show, :edit, :update, :destroy]
 
   def index
-    respond_with Card.all.order('created_at DESC')
+    @cards = Card.all.order('created_at DESC')
   end
 
   def new
@@ -10,20 +11,39 @@ class CardsController < ApplicationController
   end
 
   def create
-   
     @card = current_user.cards.build(card_params)
     if @card.save
       redirect_to cards_path
     else
-      redirect_to @card
+      redirect_to @card 
     end
   end
 
   def show
-    @card = Card.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @card.destroy
+
+    redirect_to root_path
   end
 
   private
+    def set_card
+      @card = Card.find(params[:id])
+    end
+
     def card_params
       params.require(:card).permit(:title, :body, :status)
     end
